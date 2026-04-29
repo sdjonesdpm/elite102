f05385ce0c795e96a1eeea311a6c435502e0be01
sdjonesdpm <sdjonesdpm@icloud.com>
2026-04-29 01:41:24 +0000

    deleted unnecessary file

--- show_tables.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('./bankdb')
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cursor.fetchall()
-for table in tables:
-    print(table[0])
-conn.close()
